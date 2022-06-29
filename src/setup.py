8b2e1b8d41cf499b6478e103337c61ad501e6ecf
hvrlxy <hle5@gustavus.edu>
2022-06-29 20:37:30 +0700

    add realease

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wikidichConverter',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT License',
+    description="Python Package that convert wikidich books to PDF files",
+    long_description=open('README.md').read(),
+    install_requires=[],
+    url='https://github.com/hvrlxy/wikidichConverter',
+    author='Ha Le',
+    author_email='le.ha1@northeastern.edu',
+    include_package_data=True
+)
--- src/wikidichConverter/convert.py
+++ src/wikidichConverter/convert.py
@@ -1,6 +1,6 @@
-import wikidichConverter.parse_mainpage
-import wikidichConverter.generate_pdf
-import wikidichConverter.parse_chapter
+from wikidichConverter.parse_mainpage import *
+from wikidichConverter.generate_pdf import *
+from wikidichConverter.parse_chapter import *
 
 def get_all_chapter(url: str):
     '''
@@ -8,12 +8,12 @@ def get_all_chapter(url: str):
     url: str
     return: list of str
     '''
-    mainpage = parse_mainpage.ParseMainPage(url) # parse the mainpage
+    mainpage = ParseMainPage(url) # parse the mainpage
     chapter_url = mainpage.get_first_chapter() # get the url of the first chapter
     chapter_text_lst = [] # list of chapter text
 
     while chapter_url is not None:
-        chapter = parse_chapter.ParseChapter(chapter_url) # parse the chapter
+        chapter = ParseChapter(chapter_url) # parse the chapter
         chapter_text_lst.append(chapter.get_chapter_text()) # add the chapter text to the list
         chapter_url = chapter.get_next_chapter(1) if len(chapter_text_lst) == 1 else chapter.get_next_chapter(2) # get the url of the next chapter
         # print(chapter_url)
@@ -26,9 +26,7 @@ def convert_pdf(book_url: str, file_path: str):
     file_path: str
     '''
 
+    book_name = ParseMainPage(book_url).get_book_name() # get the book name
     chapter_text_lst = get_all_chapter(book_url) # get all chapter text
-    generate_pdf.GeneratePDF(chapter_text_lst, book_url).save_pdf(file_path) # generate the pdf file
-    book_name = parse_mainpage.ParseMainPage(book_url).get_book_name() # get the book name
-    chapter_text_lst = get_all_chapter(book_url) # get all chapter text
-    obj = generate_pdf.GeneratePDF(chapter_text_lst, book_name) # generate the pdf file
+    obj = GeneratePDF(chapter_text_lst, book_name) # generate the pdf file
     obj.save_pdf(file_path)
